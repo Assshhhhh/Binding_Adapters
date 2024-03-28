93badf589b72c966ed286b6e2850aba49b39fc69
Assshhhhh <9ash09@gmail.com>
2024-03-28 17:56:18 +0500

    Initial commit

---
+++ app/src/main/java/com/example/binding_adapters/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.binding_adapters
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
+import com.example.binding_adapters.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val post = Post(
+            "Introduction to Binding Adapters",
+            "Data Binding",
+            "https://inthecheesefactory.com/uploads/source/glidepicasso/cover.jpg"
+        )
+        binding.post = post
+
+    }
+}
